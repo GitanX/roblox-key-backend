db9dcbf13d939820ce1196c94410fecf39435e38
GitanX <yooyoyo235@gmail.com>
2025-12-17 13:41:50 +0100

    Update verify.js

--- api/verify.js
+++ api/verify.js
@@ -1,18 +1,13 @@
-import { Redis } from "@upstash/redis";
-
-const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
-});
+import { kv } from "@vercel/kv";
 
 export default async function handler(req, res) {
   const { key, hwid } = req.query;
   if (!key || !hwid) return res.status(400).json({ valid: false });
 
-  const data = await redis.get(key);
+  const data = await kv.get(key);
   if (!data) return res.status(400).json({ valid: false });
 
-  const { hwid: storedHwid, expiresAt } = JSON.parse(data);
+  const { hwid: storedHwid, expiresAt } = data;
 
   if (storedHwid !== hwid) return res.status(400).json({ valid: false });
   if (Date.now() > expiresAt) return res.status(400).json({ valid: false });
